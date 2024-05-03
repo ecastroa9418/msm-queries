b946f766c497ef0410287839edee042a393787ed
ecastroa9418 <165231064+ecastroa9418@users.noreply.github.com>
2024-05-03 02:03:02 +0000

    Directors ready

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,4 +4,12 @@ class DirectorsController < ApplicationController
     render({:template => "director_templates/list"})
   end
 
+  def show
+    the_id = params.fetch("the_id")
+
+    matching_record = Director.where({:id => the_id})
+    @the_director = matching_record.at(0)
+    render({:template => "director_templates/details"})
+  end
+
 end
